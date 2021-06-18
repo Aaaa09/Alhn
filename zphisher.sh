43500443caf1b84358dfdf95e52c2e208f5ea650
Termux King <75945525+TermuxKing56@users.noreply.github.com>
2021-06-18 13:45:06 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -7,124 +7,13 @@ command -v unzip > /dev/null 2>&1 || { echo >&2 "Unzip is not installed ! Instal
 }
 banner() {
 clear
-printf " \e[33;1m ______      _     _     _               \e[0m\n"
-printf " \e[33;1m|___  /     | |   (_)   | |              \e[0m\n"
-printf " \e[33;1m   / / _ __ | |__  _ ___| |__   ___ _ __ \e[0m\n"
-printf " \e[32;1m  / /\e[33;1m | '_ \| '_ \| / __| '_ \ / _ \ '__|\e[0m\n"
-printf " \e[32;1m / /__| |_) | | | | \__ \ | | |  __/ |   \e[0m\n"
-printf " \e[32;1m/_____| .__/|_| |_|_|___/_| |_|\___|_|   \e[0m\n"
-printf " \e[33;1m      | |                                \e[0m\n"
-printf " \e[33;1m      |_|           \e[1;97m    Version 2.0      \e[0m\n"
+printf " \e[33;1m      |_|           \e[1;97m   Latest Version      \e[0m\n"
 printf " \n"
-printf " \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by Alhn (mr_______alhn)\e[0m\n"
+printf " \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by Black Team\e[0m\n"
 printf " \n"
-printf " \e[36;1m.:.Select Any Attack for your Victim.:.\e[0m\n"
+printf " \e[36;1m.:. Sorry This Tool Is Not Support In Your Device.:.\e[0m\n"
 printf " \n"
 }
-menu() {
-printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Facebook    \e[0m\e[1;31m[\e[0m\e[1;77m11\e[0m\e[1;31m]\e[0m\e[1;93m Twitch      \e[0m\e[1;31m[\e[0m\e[1;77m21\e[0m\e[1;31m]\e[0m\e[1;93m DeviantArt\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Instagram   \e[0m\e[1;31m[\e[0m\e[1;77m12\e[0m\e[1;31m]\e[0m\e[1;93m Pinterest   \e[0m\e[1;31m[\e[0m\e[1;77m22\e[0m\e[1;31m]\e[0m\e[1;93m Badoo\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Google      \e[0m\e[1;31m[\e[0m\e[1;77m13\e[0m\e[1;31m]\e[0m\e[1;93m Snapchat    \e[0m\e[1;31m[\e[0m\e[1;77m23\e[0m\e[1;31m]\e[0m\e[1;93m Origin\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m04\e[0m\e[1;31m]\e[0m\e[1;93m Microsoft   \e[0m\e[1;31m[\e[0m\e[1;77m14\e[0m\e[1;31m]\e[0m\e[1;93m Linkedin    \e[0m\e[1;31m[\e[0m\e[1;77m24\e[0m\e[1;31m]\e[0m\e[1;93m CryptoCoin\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m05\e[0m\e[1;31m]\e[0m\e[1;93m Netflix     \e[0m\e[1;31m[\e[0m\e[1;77m15\e[0m\e[1;31m]\e[0m\e[1;93m Ebay        \e[0m\e[1;31m[\e[0m\e[1;77m25\e[0m\e[1;31m]\e[0m\e[1;93m Yahoo\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m06\e[0m\e[1;31m]\e[0m\e[1;93m Paypal      \e[0m\e[1;31m[\e[0m\e[1;77m16\e[0m\e[1;31m]\e[0m\e[1;93m Dropbox     \e[0m\e[1;31m[\e[0m\e[1;77m26\e[0m\e[1;31m]\e[0m\e[1;93m Wordpress\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m07\e[0m\e[1;31m]\e[0m\e[1;93m Steam       \e[0m\e[1;31m[\e[0m\e[1;77m17\e[0m\e[1;31m]\e[0m\e[1;93m Protonmail  \e[0m\e[1;31m[\e[0m\e[1;77m27\e[0m\e[1;31m]\e[0m\e[1;93m Yandex\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m08\e[0m\e[1;31m]\e[0m\e[1;93m Twitter     \e[0m\e[1;31m[\e[0m\e[1;77m18\e[0m\e[1;31m]\e[0m\e[1;93m Spotify     \e[0m\e[1;31m[\e[0m\e[1;77m28\e[0m\e[1;31m]\e[0m\e[1;93m StackoverFlow\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m09\e[0m\e[1;31m]\e[0m\e[1;93m Playstation \e[0m\e[1;31m[\e[0m\e[1;77m19\e[0m\e[1;31m]\e[0m\e[1;93m Reddit      \e[0m\e[1;31m[\e[0m\e[1;77m29\e[0m\e[1;31m]\e[0m\e[1;93m Vk\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m10\e[0m\e[1;31m]\e[0m\e[1;93m Github      \e[0m\e[1;31m[\e[0m\e[1;77m20\e[0m\e[1;31m]\e[0m\e[1;93m Adobe       \e[0m\e[1;31m[\e[0m\e[1;77mx\e[0m\e[1;31m] \e[0m\e[1;93mExit\e[0m\n"
-printf "\e[0m\n"
-read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' option
-if [[ $option == 1 || $option == 01 ]]; then
-facebook
-elif [[ $option == 2 || $option == 02 ]]; then
-instagram
-elif [[ $option == 3 || $option == 03 ]]; then
-gmail
-elif [[ $option == 4 || $option == 04 ]]; then
-server="microsoft"
-start
-elif [[ $option == 5 || $option == 05 ]]; then
-server="netflix"
-start
-elif [[ $option == 6 || $option == 06 ]]; then
-server="paypal"
-start
-elif [[ $option == 7 || $option == 07 ]]; then
-server="steam"
-start
-elif [[ $option == 8 || $option == 08 ]]; then
-server="twitter"
-start
-elif [[ $option == 9 || $option == 09 ]]; then
-server="playstation"
-start
-elif [[ $option == 10 ]]; then
-server="github"
-start
-elif [[ $option == 11 ]]; then
-server="twitch"
-start
-elif [[ $option == 12 ]]; then
-server="pinterest"
-start
-elif [[ $option == 13 ]]; then
-server="snapchat"
-start
-elif [[ $option == 14 ]]; then
-server="linkedin"
-start
-elif [[ $option == 15 ]]; then
-server="ebay"
-start
-elif [[ $option == 16 ]]; then
-server="dropbox"
-start
-elif [[ $option == 17 ]]; then
-server="protonmail"
-start
-elif [[ $option == 18 ]]; then
-server="spotify"
-start
-elif [[ $option == 19 ]]; then
-server="reddit"
-start
-elif [[ $option == 20 ]]; then
-server="adobe"
-start
-elif [[ $option == 21 ]]; then
-server="deviantart"
-start
-elif [[ $option == 22 ]]; then
-server="badoo"
-start
-elif [[ $option == 23 ]]; then
-server="origin"
-start
-elif [[ $option == 24 ]]; then
-server="cryptocoinsniper"
-start
-elif [[ $option == 25 ]]; then
-server="yahoo"
-start
-elif [[ $option == 26 ]]; then
-server="wordpress"
-start
-elif [[ $option == 27 ]]; then
-server="yandex"
-start
-elif [[ $option == 28 ]]; then
-server="stackoverflow"
-start
-elif [[ $option == 29 ]]; then
-vk
-elif [[ $option == X || $option == x ]]; then
-exit 1
-else
-printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
-sleep 1
-banner
-menu
-fi
 }
 facebook(){
 printf " \n"
